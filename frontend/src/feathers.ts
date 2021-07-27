52aeb462da0b94c176da3cd564e934bd139db879
Rebeca Pastl <rebeca.pastl@gmail.com>
2021-07-27 02:50:03 +0000

    socket io

--- frontend/src/feathers.ts
+++ frontend/src/feathers.ts
@@ -2,7 +2,7 @@ import feathers from '@feathersjs/client';
 import socketio from '@feathersjs/socketio-client';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3030');
+const socket = io('https://book-club-2tl5upt2lq-uw.a.run.app');
 const client = feathers();
 
 client.configure(socketio(socket));
